457bdd8d4570ca3c5f14f76b5bf634ba88d19b59
nobumacsuzuki <nobumac.suzuki@gmail.com>
2020-12-14 21:11:22 -0800

    update

--- openCVTest.py
+++ openCVTest.py
@@ -3,7 +3,12 @@ import cv2
 def main():
     filename = "lena_512x512.bmp"
     gry = cv2.imread(filename, 0)
+    print(gry)
     cv2.imshow('image',gry)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    color = cv2.imread(filename, 1)
+    cv2.imshow('image',color)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
