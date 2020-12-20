2833c153161c6f799d7b52fffa59716fc5810a93
nobumacsuzuki <nobumac.suzuki@gmail.com>
2020-12-20 15:38:21 -0800

    add face recognition

--- openCVTest.py
+++ openCVTest.py
@@ -26,5 +26,29 @@ def main():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    pathCascadeEye = 'C:\\Users\\nobum\\anaconda3\\pkgs\\libopencv-4.0.1-hbb9e17c_0\\Library\\etc\\haarcascades\\haarcascade_eye.xml'
+    pathCascadeFrontFace = 'C:\\Users\\nobum\\anaconda3\\pkgs\\libopencv-4.0.1-hbb9e17c_0\\Library\\etc\\haarcascades\\haarcascade_frontalface_default.xml'
+
+    face_cascade = cv2.CascadeClassifier(pathCascadeFrontFace)
+    eye_cascade = cv2.CascadeClassifier(pathCascadeEye)
+
+    src = cv2.imread(filename)
+    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(src_gray)
+
+    for x, y, w, h in faces:
+        cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        face = src[y: y + h, x: x + w]
+        face_gray = src_gray[y: y + h, x: x + w]
+        eyes = eye_cascade.detectMultiScale(face_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(face, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    cv2.imshow('aho', src)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
     main()
