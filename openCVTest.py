b6f94c81023130fcd088ce261f9fccb4d7e2da0d
nobumacsuzuki <nobumac.suzuki@gmail.com>
2020-12-28 13:17:57 -0800

    add histogram

--- openCVTest.py
+++ openCVTest.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import sys
 import os
+from matplotlib import pyplot as plt
 
 def main():
     filename = "lena_512x512.bmp"
@@ -39,26 +40,38 @@ def main():
     pathCascadeEye = os.path.join(pathCascade, 'haarcascade_eye.xml')
     pathCascadeFrontFace = os.path.join(pathCascade,'haarcascade_frontalface_default.xml')
 
-    face_cascade = cv2.CascadeClassifier(pathCascadeFrontFace)
-    eye_cascade = cv2.CascadeClassifier(pathCascadeEye)
+    cascadeClassifierFace = cv2.CascadeClassifier(pathCascadeFrontFace)
+    cascadeClassifierEye = cv2.CascadeClassifier(pathCascadeEye)
 
-    src = cv2.imread(filename)
-    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    faces = cascadeClassifierFace.detectMultiScale(imageGray) # it returns the list of ([tartX, startY, width, height]
+    print(faces)
 
-    faces = face_cascade.detectMultiScale(src_gray)
+    colorFace = (255, 0, 0) # color is tuple in CV
+    colorEye = (0, 255, 0)
+    LineThickness = 2
 
-    for x, y, w, h in faces:
-        cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        face = src[y: y + h, x: x + w]
-        face_gray = src_gray[y: y + h, x: x + w]
-        eyes = eye_cascade.detectMultiScale(face_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(face, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+    for (facePositionX, facePositionY, faceWidth, faceHeight) in faces:
+        cv2.rectangle(imageColor, (facePositionX, facePositionY), (facePositionX + faceWidth, facePositionY + faceHeight), colorFace, LineThickness)
+        bufferFace = imageColor[facePositionY: facePositionY + faceHeight, facePositionX: facePositionX + faceWidth]
+        bufferFaceGray = imageGray[facePositionY: facePositionY + faceHeight, facePositionX: facePositionX + faceWidth]
+        eyes = cascadeClassifierEye.detectMultiScale(bufferFaceGray)
+        print(eyes)
+        for (eyePositionX, eyePositionY, eyeWidth, eyeHeight) in eyes:
+            cv2.rectangle(bufferFace, (eyePositionX, eyePositionY), (eyePositionX + eyeWidth, eyePositionY + eyeHeight), colorEye, LineThickness)
 
-    cv2.imshow('face recogntion', src)
+    cv2.imshow('face recogntion', imageColor)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    # histogram
+    imageColor = cv2.imread(filename, cv2.IMREAD_COLOR)
+    color = ('b','g','r')
+    print(color)
+    for i,col in enumerate(color):
+        histgramLena = cv2.calcHist([imageColor],[i],None,[256],[0,256])
+        plt.plot(histgramLena, color = col)
+        plt.xlim([0,256])
+    plt.show()
 
 if __name__ == "__main__":
     main()
