6652e902f7fbea0998a5f14694b6db7d3ad28c44
nobumacsuzuki <nobumac.suzuki@gmail.com>
2020-12-28 15:36:45 -0800

    update histogram

--- openCVTest.py
+++ openCVTest.py
@@ -40,7 +40,7 @@ def main():
     pathCascadeEye = os.path.join(pathCascade, 'haarcascade_eye.xml')
     pathCascadeFrontFace = os.path.join(pathCascade,'haarcascade_frontalface_default.xml')
 
-    cascadeClassifierFace = cv2.CascadeClassifier(pathCascadeFrontFace)
+    cascadeClassifierFace = cv2.CascadeClassifier(pathCascadeFrontFace) # it instatiates cascade classifier object
     cascadeClassifierEye = cv2.CascadeClassifier(pathCascadeEye)
 
     faces = cascadeClassifierFace.detectMultiScale(imageGray) # it returns the list of ([tartX, startY, width, height]
@@ -65,11 +65,17 @@ def main():
 
     # histogram
     imageColor = cv2.imread(filename, cv2.IMREAD_COLOR)
-    color = ('b','g','r')
-    print(color)
-    for i,col in enumerate(color):
-        histgramLena = cv2.calcHist([imageColor],[i],None,[256],[0,256])
-        plt.plot(histgramLena, color = col)
+    colors = ('b','g','r') #matlibplot has the color index, https://matplotlib.org/3.1.0/api/colors_api.html#module-matplotlib.colors
+    for (indexColor, color) in enumerate(colors): #enumerate provides list of object with index, in this case [(0, 'b'), (1, 'g'), (2, 'r')] 
+        # https://docs.opencv.org/master/d1/db7/tutorial_py_histogram_begins.html
+        # cv.calcHist(images, channels, mask, histSize, ranges[, hist[, accumulate]])
+        # images : it is the source image of type uint8 or float32. it should be given in square brackets, ie, "[img]".
+        # channels : it is also given in square brackets. It is the index of channel for which we calculate histogram. For example, if input is grayscale image, its value is [0]. For color image, you can pass [0], [1] or [2] to calculate histogram of blue, green or red channel respectively.
+        # mask : mask image. To find histogram of full image, it is given as "None". But if you want to find histogram of particular region of image, you have to create a mask image for that and give it as mask. (I will show an example later.)
+        # histSize : this represents our BIN count. Need to be given in square brackets. For full scale, we pass [256].
+        # ranges : this is our RANGE. Normally, it is [0,256].
+        bufferHistogram = cv2.calcHist([imageColor],[indexColor],None,[256],[0,256]) # it retunrs list of channel histogram
+        plt.plot(bufferHistogram, color = color)
         plt.xlim([0,256])
     plt.show()
 
